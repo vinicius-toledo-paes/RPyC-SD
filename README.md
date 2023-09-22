acbba024264e8b49e8db6b8b2debe6c521cc71ec
Mauro <maurobueno@id.uff.br>
2023-09-22 19:50:29 -0300

    Revert "questao1-2"
    This reverts commit bcf6e02ac1c52578f7b324ac647ccde2cd129550.

--- README.md
+++ README.md
@@ -1,5 +1,2 @@
 Bibilioteca rpyc
 intallar por: pip install rpyc
-
-
-para conectar no cliente python .\client.py "localhost"
--- client.py
+++ client.py
@@ -1,18 +1,10 @@
 import rpyc
 import sys
-#1)
-# if len(sys.argv) < 2:
-#     exit("Usage {} SERVER".format(sys.argv[0]))
-# server = sys.argv[1]
-# conn = rpyc.connect(server,18861)
-# print(conn.root)
-# print(conn.root.get_answer())
-# print(conn.root.the_real_answer_though) 
 
-#2)
-import os
 if len(sys.argv) < 2:
     exit("Usage {} SERVER".format(sys.argv[0]))
 server = sys.argv[1]
 conn = rpyc.connect(server,18861)
-print(c.get_question)
+print(conn.root)
+print(conn.root.get_answer())
+print(conn.root.the_real_answer_though) 
